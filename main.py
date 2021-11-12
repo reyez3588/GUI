2f2315e9cb0bf198da6b3b49e5c8dae2ba161020
reyez3588 <reyez3588@gmail.com>
2021-11-12 13:29:42 -0600

    Dia 13 de noviembre

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 # Only needed for access to command line arguments
 import sys
-#Necesarias para cargar librerias QT
+
+
+#Necesarias para cargar librerias pyQT5 (pyside2 es muy utilizada)
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 
 
-class mainWindow(QMainWindow):
+class miNuevaVentana(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setFixedSize(QSize(400, 300))
-        self.setWindowTitle("My first GUI")
-        button = QPushButton("Push One")
+        self.setFixedSize(QSize(500, 250))
+        self.setWindowTitle("Clase de programación avanzada")
+
+        button = QPushButton("Presiona aquí!!!")
         self.setCentralWidget(button) #agregar el botón creado a la ventana principal
 
 # You need one (and only one) QApplication instance per application.
@@ -21,8 +24,8 @@ app = QApplication(sys.argv)
 # Create a Qt widget, which will be our window. any could be a window
 #window = QWidget()
 #window = QPushButton(" Botototon ")
-window = mainWindow()
-window.show() # IMPORTANT!!!!! Windows are hidden by default.
+ventanaPrincipal = miNuevaVentana()
+ventanaPrincipal.show() # IMPORTANT!!!!! Windows are hidden by default.
 
 # Start the event loop.
 app.exec_()
